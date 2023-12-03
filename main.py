271687b639b9043e1a4827d06b50ca410f1b6678
Alberto Venzo <alberto.venzo95@gmail.com>
2023-12-04 00:46:13 +0100

    set different colormode

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle as t
-from random import random
+import random
 import math
 
 timmy = t.Turtle()
@@ -11,9 +11,9 @@ print(f"Speed set to {speed}")
 
 for sides in range(3,11):
     angle = 360/sides
-    red = random()
-    blue = random()
-    green = random()
+    red = random.random()
+    blue = random.random()
+    green = random.random()
     timmy.pencolor(red, blue, green)
     print(f"RGB color for {sides} sides polygon is: {red:.2f}, {blue:.2f}, {green:.2f}")
     for n in range(sides):
@@ -26,12 +26,13 @@ timmy.clear()
 timmy.pensize(3)
 dash_length = 10
 number_of_iterations = 500
+timmy.screen.colormode(255) 
 
 for i in range(number_of_iterations):
-    angle = 90*(math.ceil(random()*4))
-    red = random()
-    blue = random()
-    green = random()
+    angle = 90*(math.ceil(random.random()*4))
+    red = random.randint(0,255)
+    blue = random.randint(0,255)
+    green = random.randint(0,255)
     timmy.pencolor(red, blue, green)
     print(f"Random angle in iteration {i} is: {angle})")
     timmy.right(angle)
